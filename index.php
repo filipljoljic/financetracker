ff898c0e445faa4f0ab092cb9e77090d0ab6ff71
filipljoljic <filip.ljoljic@stu.ibu.edu.ba>
2023-03-02 10:47:54 +0100

    deleting unnecessary files

--- index.php
+++ index.php
@@ -6,14 +6,11 @@ error_reporting(E_ALL);
 require_once ("vendor/autoload.php");
 require_once ("rest/dao/FinanceDao.class.php");
 
-$dao = new FinanceDao();
-$results = $dao->get_all(); 
-print_r($results);
 
-// Flight::route('/', function(){
-//     echo 'Hello its me Filip';
-// });
+Flight::route('/', function(){
+    echo 'Hello its me Filip';
+});
 
-// Flight::start();
+Flight::start();
 
 ?>  
