36bc1f167f140d1a13f2ad976c0892aab8697e15
filipljoljic <filip.ljoljic@stu.ibu.edu.ba>
2023-03-02 11:03:35 +0100

    form gitignore

---
+++ rest/dao/FinanceDao.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class FinanceDao{
+
+    private $conn;
+
+    /*constructor of dao class
+     */
+    public function __construct(){
+
+    $servername = "localhost";
+    $username = "financetracker";
+    $password = "financetracker";
+    $schema = "finance";
+    $this->conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+    // set the PDO error mode to exception
+    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+    /* METHOD Read all fils from db */
+    public function get_all(){
+        $stmt = $this->conn->prepare("SELECT * FROM finances");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /* METHOD Inserting files intop the db */
+    public function add($description, $created){
+        $stmt = $this->conn->prepare("INSERT INTO finances (description, created) VALUES (:description, :created)");
+        $stmt->execute(['description' => $description, 'created' => $created]);
+
+    }
+
+    /* Delete recorf from db */
+    public function delete($id){
+        $stmt = $this->conn->prepare("DELETE from finances where id=:id");
+        $stmt->bindParam(':id',$id); //sql injection prevention
+        $stmt->execute();
+    }
+    /*Update finance records in db */
+    public function update($id, $description, $created){
+        $stmt = $this->conn->prepare("UPDATE finances SET description=:description, created=:created WHERE id=:id");
+        $stmt->execute(['id'=> $id, 'description' => $description, 'created' => $created]);
+    }
+}
+
+?>
