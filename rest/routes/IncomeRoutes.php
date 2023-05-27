7f5ef7ae3b13d38abb021e33b7203a13321bc1ec
muky1 <muhamed.begic01@gmail.com>
2023-05-27 23:08:57 +0200

    Implement API endpoints for income

---
+++ rest/routes/IncomeRoutes.php
@@ -0,0 +1,35 @@
+<?php
+/*
+GET /users/{id}/income: Retrieves a list of all income entries for a user.
+GET /users/{id}/income/{income_id}: Retrieves a specific income entry.
+POST /users/{id}/income: Creates a new income entry.
+PUT /users/{id}/income/{income_id}: Updates an existing income entry.
+DELETE /users/{id}/income/{income_id}: Deletes an income entry.
+*/
+
+Flight::route("GET /users/@id/income", function(){
+    Flight::json(Flight::incomeDao()->get_all());
+});
+
+Flight::route("GET /users/@id/income/@income_id", function($id, $income_id){
+    Flight::json(Flight::incomeDao()->get_by_id($income_id));
+});
+
+Flight::route('POST /users/@id/income', function($id){
+    $data = Flight::request()->data->getData();
+    $data['user_id'] = $id;
+    Flight::json(Flight::incomeDao()->addIncome($data));
+});
+
+Flight::route('PUT /users/@id/income/@income_id', function($id, $income_id) {
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::incomeDao()->updateIncome($data, $income_id));
+});
+
+Flight::route('DELETE /users/@id/income/@income_id', function($income_id) {
+    Flight::json(Flight::incomeDao()->deleteIncome($income_id), 
+                    ['message' => 'Income entry deleted']);
+
+});
+
+?>
