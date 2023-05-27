c8df06fff617a26bc2e78b8e2da3ea274e9f71a3
muky1 <muhamed.begic01@gmail.com>
2023-05-27 23:13:29 +0200

    Remove unused methods

--- rest/index.php
+++ rest/index.php
@@ -12,48 +12,6 @@ Flight::register('baseDao','BaseDao');
 Flight::register("userDao", "UserDao");
 Flight::register("incomeDao", "IncomeDao");
 
-/* METHODS */
-Flight::route('GET /finances', function(){
-    Flight::json(Flight::baseDao()->get_all());
-});
-
-// single finances
-Flight::route('GET /finances/@id', function($id){
-    Flight::json(Flight::baseDao()->get_by_id($id));
-});
-
-//Add finances
-Flight::route('POST /finances', function(){
-    Flight::json(Flight::baseDao()->add(Flight::request()->data->getData()));
-    
-});
-
-//Delete finances
-Flight::route('DELETE /finances/@id', function($id){
-    Flight::baseDao()->delete($id);
-    Flight::json(["message"=>"deleted"]);
-});
-
-//Update finances
-Flight::route('PUT /finances/@id', function($id){
-    $data = Flight::request()->data->getData();
-    $data['id'] = $id;
-    Flight::json(Flight::baseDao()->update($data));
-});
-
-Flight::route("GET /users", function() {
-    Flight::json(Flight::userDao()->get_all());
-});
-
-
-Flight::route('/', function(){
-    echo 'Hello';
-});
-
-Flight::route("/testing", function() {
-    echo "This is a test route to see if routing works properly!";
-});
-
 // require routes in index
 require_once 'routes/UserRoutes.php';
 require_once "routes/IncomeRoutes.php";
