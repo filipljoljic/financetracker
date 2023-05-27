656a746613ce0d8f75eb1baff0c7ae9e7eaaf448
muky1 <muhamed.begic01@gmail.com>
2023-05-27 23:10:14 +0200

    Implement methods to IncomeDao for db interaction

--- rest/dao/IncomeDao.class.php
+++ rest/dao/IncomeDao.class.php
@@ -7,8 +7,27 @@ class IncomeDao extends BaseDao {
         parent::__construct("income");
     }
 
-    public function get_all(){
-        return parent::get_all();
+    public function get_income_by_user_id($user_id) {
+        $stmt = $this->conn->prepare("SELECT * FROM " . $this->table_name . " WHERE user_id=:user_id");
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    }
+
+    public function getIncomeById($id) {
+        return parent::get_by_id($id);
+    }
+
+    public function addIncome($income) {
+        return parent::add($income);
+    }
+
+    public function updateIncome($income, $id) {
+        return parent::update($income, $id);
+    }
+
+    public function deleteIncome($id) {
+        return parent::delete($id);
     }
 }
 ?>
