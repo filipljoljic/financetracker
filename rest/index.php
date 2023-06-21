d63355ffa40f4ffc5ca423fde5bfa0b54f20d579
muky1 <muhamed.begic01@gmail.com>
2023-06-21 23:10:06 +0200

    Register expense dao and include routes

--- rest/index.php
+++ rest/index.php
@@ -7,13 +7,16 @@ require_once ("../vendor/autoload.php");
 require_once ("dao/BaseDao.class.php");
 require_once ("dao/UserDao.class.php");
 require_once ("dao/IncomeDao.class.php");
+require_once ("dao/ExpenseDao.class.php");
 
 Flight::register('baseDao','BaseDao');
 Flight::register("userDao", "UserDao");
 Flight::register("incomeDao", "IncomeDao");
+Flight::register("expenseDao", "ExpenseDao");
 
 // require routes in index
 require_once 'routes/UserRoutes.php';
 require_once "routes/IncomeRoutes.php";
+require_once "routes/ExpenseRoutes.php";
 
 Flight::start();
